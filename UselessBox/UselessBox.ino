dbd0b0278d4c5d7fc214e74cf95841cec727be87
Wolfgang Lohwasser <wlohwass@wolfix.fritz.box>
2020-12-05 20:27:21 +0100

    New servo frame time hangling. Immediate response.

--- UselessBox/UselessBox.ino
+++ UselessBox/UselessBox.ino
@@ -1,11 +1,20 @@
 /*
  * UselessBox - A useless box.
- * 
+ *
+ * 02-12-2020 - V0.9.2, changed servo frame time handling
+ * 20-11-2020 - V0.9.1, added immediate response
+ * 29-11-2020 - V0.9, basically working
  */
 
 #include <Servo.h>
 
-#define DEBUG_ON
+
+/* Dump some stuff to Serial if this is defined. */
+#undef DEBUG_ON
+
+/* Use millis() instead of random() */
+#define USE_MILLIS_AS_RANDOM
+
 
 /* Arduino pins */
 #define DOOR_PIN    ((byte) 9)
@@ -17,73 +26,122 @@
 #define UP_PIN      ((byte) 6)
 #define DOWN_PIN    ((byte) 5)
 
+/* Determine switch state */
+#define SWITCH_ON   (switchOn( SWITCH_PIN))
+#define SWITCH_OFF  (!SWITCH_ON)
+
+
 /* Response commands */
 #define C_END       ((byte) 0)
 #define C_WAIT      ((byte) 1)
 #define C_DOOR      ((byte) 2)
 #define C_FINGER    ((byte) 3)
 
-/* Speed definitions */
+/* Speed definitions for door and finger moves. */
 #define SLOW        ((byte) 0)
 #define NORMAL      ((byte) 1)
 #define FAST        ((byte) 2)
-#define HYPER       ((byte) 3)
+#define WARP        ((byte) 3)
 
-static int speedIndex[4] { 3, 10, 25, 40 };
+/* Speed index used for SLOW,NORMAL,FAST,WARP speed.
+ * Microsecond increase/decrease per servo frame time (around 20msec).
+ */
+static int speedIndex[4] {
+  3,  // SLOW
+  8,  // NORMAL
+  20, // FAST
+  40  // WARP
+};
 
-/*
- * tim     time in 100msec units (up to 63 = 6.3 seconds)
- * pos     position 0 - 15
- * spd     speed as defined above
+/* Move sequence commands.
+ * Every command is encoded in a single byte.
+ * 
+ * Parameters:
+ *  tim     time in 100msec units (up to 63 = 6.3 seconds)
+ *  pos     position 0 - 15
+ *  spd     speed as defined above
  */
 #define END               (C_END << 6)
 #define WAIT( tim)       ((C_WAIT << 6)   | (tim & 0b00111111))
 #define DOOR( pos, spd)  ((C_DOOR << 6)   | ((pos & 0b00001111) << 2) | spd)
 #define FINGER( pos,spd) ((C_FINGER << 6) | ((pos & 0b00001111) << 2) | spd)
 
+/* Macros to retrieve values from move sequence commands.
+ */
 #define GET_CMD( resp)   ((resp & 0b11000000) >> 6)
 #define GET_TIM( resp)   (resp & 0b00111111)
 #define GET_POS( resp)   ((resp & 0b00111100) >> 2)
 #define GET_SPD( resp)   (resp & 0b00000011)
 
-/* DO NOT FORGET TO TERMINATE EVERY SEQUENCE WITH "END" */
-static byte moveSeq1[] = { WAIT( 10), DOOR( 15, NORMAL), FINGER( 15, FAST), WAIT( 2), FINGER( 0, FAST), DOOR( 0, HYPER), END };
-static byte moveSeq2[] = { WAIT( 10), FINGER( 15, FAST), FINGER( 0, FAST), END };
-static byte moveSeq3[] = { WAIT( 10), DOOR( 7, SLOW), WAIT( 15), DOOR( 0, FAST), WAIT( 5), FINGER( 15, FAST), FINGER( 0, HYPER), END };
 
+/* Move sequences.
+ *
+ * DO NOT FORGET TO TERMINATE EVERY SEQUENCE WITH "END"
+ */
+static byte moveAggressive[] = { DOOR( 10, FAST), FINGER( 15, FAST), FINGER( 0, FAST), DOOR( 0, WARP), END };
+static byte moveNormal[] = { DOOR( 15, NORMAL), FINGER( 15, FAST), WAIT( 2), FINGER( 0, FAST), DOOR( 0, WARP), END };
+static byte moveShy[] = { DOOR( 8, SLOW), WAIT( 15), DOOR( 0, FAST), WAIT( 5), DOOR( 10, FAST), FINGER( 15, FAST), FINGER( 0, WARP), DOOR( 0, WARP), END };
+static byte moveSeq4[] = { DOOR( 15, NORMAL), FINGER( 10, SLOW), WAIT( 5), FINGER( 15, WARP), FINGER( 10, WARP), WAIT( 10), FINGER(0, NORMAL), DOOR( 0, WARP), END };
+static byte moveSeq5[] = { DOOR( 15, NORMAL), FINGER( 10, SLOW), WAIT( 5), FINGER( 15, WARP), FINGER( 0, WARP), DOOR( 0, WARP), END };
+
+/* The list of move sequences to be used in a random order. */
+static byte *moveSequences[] = {
+  moveAggressive,
+  moveNormal,
+  moveShy,
+  moveSeq4,
+  moveSeq5
+};
+
+
+/* Configuration stored in EEPROM.
+ * Stores start and end positions of door and finger.
+ */
+ 
 typedef uint16_t checksum_t;
 
-typedef struct Config {
-    
+typedef struct config_t {
+
   int door_minPos;
   int door_maxPos;
   int finger_minPos;
   int finger_maxPos;
-  
+
   checksum_t checksum;
 
-} Config;
+} config_t;
 
 #define EEPROM_START_LOC    ((uint8_t*)0)
-#define CONFIG_SIZE_BYTES   (sizeof(Config) - sizeof(checksum_t))
+#define CONFIG_SIZE_BYTES   (sizeof(config_t) - sizeof(checksum_t))
+
 
 /* */
-typedef struct ServoStruct {
+typedef struct servostruct_t {
 
   int minPos;
   int maxPos;
   int curPos;
 
-  Servo servo;
+  unsigned long nextFrame_msec;
   
-} ServoStruct;
+  Servo servo;
 
-#define SERVO_MID_POS       ((int)1500)
-#define SERVO_FRAME_MSEC    ((int)18)
+} servostruct_t;
 
-ServoStruct door;
-ServoStruct finger;
-Config configuration;
+#define SERVO_MID_POS_usec  ((int)1500)
+
+/* Might need to set this to 20 for slow analog servos */
+#define SERVO_FRAME_msec    ((int)11)
+
+
+/***** global variables ************************/
+
+servostruct_t door;
+servostruct_t finger;
+config_t configuration;
+
+
+/***********************************************/
 
 void setup() {
 
@@ -97,62 +155,89 @@ void setup() {
   pinMode( UP_PIN, INPUT_PULLUP);
   pinMode( DOWN_PIN, INPUT_PULLUP);
 
+#ifndef USE_MILLIS_AS_RANDOM
+  randomSeed(analogRead(0));
+#endif
+
   initServo( door, DOOR_PIN);
   initServo( finger, FINGER_PIN);
-    
+
+  /* Read configuration from EEPROM.
+   * If it is invalid flash the LED 5 times and do a manual configuration.
+   */
   readConfig( configuration);
-  if( !isConfigValid(configuration)) {
+  if ( !isConfigValid(configuration)) {
     flash( 5, 100);
     doConfig( configuration);
   }
 
-  configServo( door, 
-    configuration.door_minPos,
-    configuration.door_maxPos);
+  /* Configure end positions for door and finger moves.  
+   */
+  configServo( door,
+               configuration.door_minPos,
+               configuration.door_maxPos);
 
-  configServo( finger, 
-    configuration.finger_minPos,
-    configuration.finger_maxPos);
-
-  randomSeed(analogRead(0));
+  configServo( finger,
+               configuration.finger_minPos,
+               configuration.finger_maxPos);
 }
 
 void loop() {
 
-  if( keyPressed( CONFIG_PIN)) {
-    
+  /* If the config key is pressed force a manual configuration
+   * and store it on EEPROM.
+   */
+  if ( keyPressed( CONFIG_PIN)) {
+
     doConfig( configuration);
 
-    configServo( door, 
-      configuration.door_minPos,
-      configuration.door_maxPos);
+    configServo( door,
+                 configuration.door_minPos,
+                 configuration.door_maxPos);
 
-    configServo( finger, 
-      configuration.finger_minPos,
-      configuration.finger_maxPos);
+    configServo( finger,
+                 configuration.finger_minPos,
+                 configuration.finger_maxPos);
   }
 
-  if( !digitalRead( SWITCH_PIN)) {
-    byte r = (byte)random(10);
+  if ( SWITCH_ON) {
 
-    if( r < 4) {
-      react( moveSeq1);
-    } else if( r < 8) {
-      react( moveSeq2);
-    } else {
-      react( moveSeq3);
-    }
+#ifdef USE_MILLIS_AS_RANDOM
+    byte rnd = (byte)millis();
+#else
+    byte rnd = (byte)random(255);
+#endif
+
+    byte sequenceCount = sizeof(moveSequences)/sizeof(byte*);
+    rnd %= sequenceCount;
+
+#ifdef DEBUG_ON
+    Serial.print("cnt ");
+    Serial.print(sequenceCount);
+    Serial.print(" rnd ");
+    Serial.println(rnd);
+#endif
+
+    react( moveSequences[rnd]);
   }
 }
 
+/* React when the switch gets flipped to ON state 
+ */
 static void react( byte seq[]) {
 
   byte index = 0;
   byte command;
-  
-  while( (command = seq[index++]) != END) {
-    
-    switch( GET_CMD( command)) {
+
+  /* immediateReact is set when the switch is flipped back to ON state
+   * while the current react sequence is still processed.
+   * In this case we want to immediately react with an aggressive sequence.
+   */
+  boolean immediateReact = false;
+
+  while ( (command = seq[index++]) != END) {
+
+    switch ( GET_CMD( command)) {
       case C_END:  // should never come here
         break;
 
@@ -161,70 +246,100 @@ static void react( byte seq[]) {
         break;
 
       case C_DOOR: // move door
-        moveServo( door, GET_POS( command), GET_SPD( command));
+        immediateReact = moveServo( door, GET_POS( command), GET_SPD( command));
         break;
 
       case C_FINGER: // move finger
-        moveServo( finger, GET_POS( command), GET_SPD( command));
+        immediateReact = moveServo( finger, GET_POS( command), GET_SPD( command));
         break;
-    }    
-  }    
+    }
+
+    if ( immediateReact) {
+      index = 0;
+      seq = moveAggressive;
+    }
+  }
 }
 
-static void moveServo( ServoStruct &s, byte pos, byte spd) {
+/* Wait until the previous servo frame time has passed,
+ * then set new servo timing value.
+ */
+static void writeServo( servostruct_t &s, int time_usec) {
 
-  int endPos = s.minPos + ((s.maxPos - s.minPos) / 15) * pos;
-  int delta = (endPos - s.curPos);
-  int deltaPerStep = speedIndex[spd];
-  int steps;
+  unsigned long now = millis();
 
-  if( delta < 0) deltaPerStep = -deltaPerStep;
-
-  steps = delta / deltaPerStep;
-
-  for(int i=0; i < steps; i++) {
-    s.curPos += deltaPerStep;
-    s.servo.writeMicroseconds( s.curPos);
-    delay( SERVO_FRAME_MSEC);
+  if( now < s.nextFrame_msec) {
+    delay( s.nextFrame_msec - now);
   }
   
-  s.curPos = endPos;
-  s.servo.writeMicroseconds( s.curPos);
-  delay( SERVO_FRAME_MSEC);
+  s.servo.writeMicroseconds( time_usec);
+
+  /* @TODO: This rolls over after about 50 days */
+  s.nextFrame_msec = now + SERVO_FRAME_msec;
 }
 
-static void initServo( ServoStruct &s, byte pin) {
-  
-  s.minPos = SERVO_MID_POS;
-  s.maxPos = SERVO_MID_POS;
-  s.curPos = SERVO_MID_POS;
+/* Move servo from current position to new position with a defined speed.
+ */
+static boolean moveServo( servostruct_t &s, byte newPos, byte howFast) {
+
+  int endPos = s.minPos + ((s.maxPos - s.minPos) / 15) * newPos;
+  int delta_usec = (endPos - s.curPos);
+  int usecPerStep = (delta_usec < 0) ? -speedIndex[howFast] : speedIndex[howFast];
+  int steps = delta_usec / usecPerStep;
+
+  boolean switchWasOff = SWITCH_OFF;
+
+  for (int i = 0; i < steps; i++) {
+    s.curPos += usecPerStep;
+    writeServo( s, s.curPos);
+
+    if ( switchWasOff && SWITCH_ON) {
+      return true;
+    }
+  }
+
+  s.curPos = endPos;
+  writeServo( s, s.curPos);
+
+  return false;
+}
+
+static void initServo( servostruct_t &s, byte pin) {
+
+  s.minPos = SERVO_MID_POS_usec;
+  s.maxPos = SERVO_MID_POS_usec;
+  s.curPos = SERVO_MID_POS_usec;
+  s.nextFrame_msec = millis() + SERVO_FRAME_msec;
+
   s.servo.attach(pin);
 }
 
-static void configServo( ServoStruct &s, int minP, int maxP) {
-  
+static void configServo( servostruct_t &s, int minP, int maxP) {
+
   s.minPos = minP;
   s.maxPos = maxP;
   s.curPos = minP;
-  s.servo.writeMicroseconds(s.curPos);
-  delay( SERVO_FRAME_MSEC);
+
+  writeServo( s, s.curPos);
 }
 
-static void flash( byte howOften, int howFast) {
-  
-  for( byte i=0; i<howOften; i++) {
+/* Flash the LED a couple of times.
+ */
+static void flash( byte howOften, int howLong) {
+
+  for ( byte i = 0; i < howOften; i++) {
     digitalWrite(LED_PIN, HIGH);
-    delay(howFast);
+    delay(howLong);
     digitalWrite(LED_PIN, LOW);
-    delay(howFast);
+    delay(howLong);
   }
 }
 
 static boolean keyPressed( byte pin) {
 
-  if( !digitalRead(pin)) {
+  if ( !digitalRead(pin)) {
     delay( 50);
-    while( !digitalRead(pin)) {
+    while ( !digitalRead(pin)) {
       delay( 10);
     }
     delay( 50);
@@ -234,17 +349,38 @@ static boolean keyPressed( byte pin) {
   return false;
 }
 
-static void doConfig( Config &cfg) {
-  
-  while( !keyPressed( CONFIG_PIN)) {
-    door.servo.writeMicroseconds(SERVO_MID_POS);
-    finger.servo.writeMicroseconds(SERVO_MID_POS);
-    delay( SERVO_FRAME_MSEC);
+/* Determine switch state.
+ * Wait until we see the same state 5 times in a row.
+ * This function takes at least 5 milliseconds.
+ */
+static boolean switchOn( byte pin) {
+
+  boolean state = false;
+  byte count = 0;
+
+  while ( count < 5) {
+    if ( !digitalRead(pin) == state) {
+      delay( 1);
+      count++;
+    } else { // switch state
+      state = !state;
+      count = 0;
+    }
   }
-  
+
+  return state;
+}
+
+static void doConfig( config_t &cfg) {
+
+  while ( !keyPressed( CONFIG_PIN)) {
+    writeServo( door, SERVO_MID_POS_usec);
+    writeServo( finger, SERVO_MID_POS_usec);
+  }
+
   flash( 1, 200);
   cfg.door_minPos = kbdMove( door, cfg.door_minPos);
-  
+
   flash( 2, 200);
   cfg.door_maxPos = kbdMove( door, cfg.door_maxPos);
 
@@ -258,34 +394,33 @@ static void doConfig( Config &cfg) {
   writeConfig( cfg);
 }
 
-static int kbdMove( ServoStruct &s, int pos) {
-  
-  s.servo.writeMicroseconds(pos);
-  delay( SERVO_FRAME_MSEC);
+static int kbdMove( servostruct_t &s, int pos) {
 
-  while( !keyPressed( CONFIG_PIN)) {
-    
-    if( !digitalRead(DOWN_PIN)) {    
+  writeServo( s, pos);
+
+  while ( !keyPressed( CONFIG_PIN)) {
+
+    if ( !digitalRead( DOWN_PIN)) {
       pos -= 1;
-      s.servo.writeMicroseconds(pos);
-      delay( SERVO_FRAME_MSEC);
-      
-    } else if( !digitalRead(UP_PIN)) {   
+      writeServo( s, pos);
+
+    } else if ( !digitalRead( UP_PIN)) {
       pos += 1;
-      s.servo.writeMicroseconds(pos);
-      delay( SERVO_FRAME_MSEC);
+      writeServo( s, pos);
     }
   }
 
   return pos;
 }
 
-static void readConfig( Config &cfg) {
+/* Read configuration from EEPROM.
+ */
+static void readConfig( config_t &cfg) {
 
   void *configStart;
 
   configStart = (void*)(EEPROM_START_LOC);
-  eeprom_read_block( &cfg, configStart, sizeof(Config));
+  eeprom_read_block( &cfg, configStart, sizeof(config_t));
 
 #ifdef DEBUG_ON
   Serial.println("read");
@@ -297,8 +432,10 @@ static void readConfig( Config &cfg) {
 #endif
 }
 
-static void writeConfig(Config &cfg) {
-    
+/* Update the checksum and write configuration to EEPROM.
+ */
+static void writeConfig( config_t &cfg) {
+
   void *configStart;
 
   configStart = (void*)(EEPROM_START_LOC);
@@ -313,31 +450,33 @@ static void writeConfig(Config &cfg) {
   Serial.println(cfg.checksum);
 #endif
 
-  eeprom_update_block( &cfg, configStart, sizeof(Config));
+  eeprom_update_block( &cfg, configStart, sizeof(config_t));
 }
 
-static checksum_t computeChecksum( Config &cfg)
+static checksum_t computeChecksum( config_t &cfg)
 {
-    checksum_t checksum = 0;
-    byte *p = (byte*)(&cfg);
+  checksum_t checksum = 0;
+  byte *p = (byte*)(&cfg);
 
-    for( checksum_t i = 0; i < CONFIG_SIZE_BYTES; i++)
-    {
-        checksum = rotate(checksum);
-        checksum ^= (i ^ ((byte)*(p+i)));
-    }
+  for ( checksum_t i = 0; i < CONFIG_SIZE_BYTES; i++)
+  {
+    checksum = rotate(checksum);
+    checksum ^= (i ^ ((byte) * (p + i)));
+  }
 
-    return checksum;
+  return checksum;
 }
 
 /* Left rotate unsigned int value.
  */
 static checksum_t rotate( checksum_t v)
 {
-    return ((v >> 15) & 1) | (v << 1);
+  return ((v >> 15) & 1) | (v << 1);
 }
 
-static boolean isConfigValid( Config &cfg)
+/* Return true if the configuration checksum is korrect.
+ */
+static boolean isConfigValid( config_t &cfg)
 {
-    return cfg.checksum == computeChecksum( cfg);
+  return cfg.checksum == computeChecksum( cfg);
 }
